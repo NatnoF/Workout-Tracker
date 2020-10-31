3b2c7537a6b7e9f74589499f4322ba7802e2c987
Nicolas Fontan <nico.fontan1@gmail.com>
2020-10-30 20:01:26 -0400

    Deploy fix, made the port process.env.port or 3000 so that the port heroku uses actually works.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -18,7 +18,7 @@ mongoose.connect(
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
+    useCreateIndex: true,
     useFindAndModify: false
   }
 );
